f976901dbfbd2187bb6ed2083592297d179db833
samat <soma.sultan@mail.ru>
2023-01-26 23:54:51 +0300

    v0.9

--- main.py
+++ main.py
@@ -1,60 +1,66 @@
 from ftplib import FTP
-from time import sleep
+import time
 import telebot
 from config import *
 import logging
 
 bot = telebot.TeleBot(TOKEN, threaded=False)
-
-
-def checking():
-    dir_anonc = './/Анонсы'
-    dir_osn = './'
-    ftp = FTP(ip)
-    ftp.login(user=userr, passwd=passwrd)
-    first_prev = set(ftp.nlst(dir_anonc))
-    sec_prev = set(ftp.nlst(dir_osn))
-    ftp.quit()
-    sleep(5)  # так нужно
-    k = 100
-    while True:
-        ftp = FTP(ip)
-        ftp.login(user=userr, passwd=passwrd)
-        if k == 100:
-            bot.send_message(id_sms_telegram, 'присоединился к ФТП ')
-            k = 5
-        first = set(ftp.nlst(dir_anonc))
-        add, rem = first - first_prev, first_prev - first
-        if add or rem:
-            yield add, rem
-        first_prev = first
-        second = set(ftp.nlst(dir_osn))
-        add_sec, rem_sec = second - sec_prev, sec_prev - second
-        if add_sec or rem_sec:
-            yield add_sec, rem_sec
-        sec_prev = second
-        sleep(180) # так тоже нужно
-
-
 LOG_FILENAME = 'loger.log'
 logging.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s',
                     filename=LOG_FILENAME, level=logging.INFO, encoding='UTF-8')
-logging.info('start bot')
 
-for add, rem in checking():
-    s, g = '', ''
-    s += ('\n'.join('+ %s' % i for i in add))  # добавление файла
-    g += ('\n'.join('- %s' % i for i in rem))  # удаление фала
-    logging.info(s)
-    logging.info(g)
-    bot.send_message(id_sms_telegram, s)
 
-for add_sec, rem_sec in checking():
-    s, g = '', ''
-    s += ('\n'.join('+ %s' % i for i in add_sec))  # добавление файла
-    g += ('\n'.join('- %s' % i for i in rem_sec))  # удаление фала
-    logging.info(s)
-    logging.info(g)
-    bot.send_message(id_sms_telegram, s)
+def checking(first_prev, sec_prev):  # есть предположение, что падает раз в 6 дней
+    dir_anonc = './/Анонсы'
+    dir_osn = './'
+    s, s1 = '', ''
+    ftp = FTP(ip)
+    ftp.login(user=userr, passwd=passwrd)
+    first = set(ftp.nlst(dir_anonc))
+    second = set(ftp.nlst(dir_osn))
+    add = first - first_prev
+    add_sec = second - sec_prev
+    s += ('\n'.join('+ %s' % i for i in add))
+    s1 += ('\n'.join('+ %s' % i for i in add_sec))
+    while s == "" or s1 == "":
+        first = set(ftp.nlst(dir_anonc))
+        second = set(ftp.nlst(dir_osn))
+        add = first - first_prev
+        add_sec = second - sec_prev
+        s += ('\n'.join('+ %s' % i for i in add))
+        s1 += ('\n'.join('+ %s' % i for i in add_sec))
+        time.sleep(180)
+        if s != '' or s1 != '':
+            return s, s1
+    else:
+        return s, s1
 
-# ftp.quit()
+
+# надо завернуть в мэйн=нэйм, ебануть еще один цикл и не сойти с ума
+def restart():
+    try:
+        dir_anonc = './/Анонсы'
+        dir_osn = './'
+        ftp = FTP(ip)
+        ftp.login(user=userr, passwd=passwrd)
+        first_prev = set(ftp.nlst(dir_anonc))
+        sec_prev = set(ftp.nlst(dir_osn))
+        # time.sleep(10)
+        ftp.quit()
+        s, s1 = checking(first_prev, sec_prev)
+        if s != '':
+            bot.send_message(id_sms_telegram, s)
+            logging.info(s)
+        if s1 != '':
+            bot.send_message(id_sms_telegram, s1)
+            logging.info(s1)
+        restart()
+    except Exception as e:
+        logging.info(e)
+        restart()
+
+
+if __name__ == '__main__':
+    bot.send_message(id_sms_telegram, 'bot started on test mode')
+    logging.info('start main_test.pyw')
+    restart()
